a03f5c108963fec435aa4598e876ec3919b118a8
MrwanTarik <mrwantarik50@gmail.com>
2024-07-24 00:28:21 +0300

    updated select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/offers/add-offer/add-offer.component.ts
+++ src/app/offers/add-offer/add-offer.component.ts
@@ -1,18 +1,22 @@
-import { NgClass, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-offer',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './add-offer.component.html',
   styleUrl: './add-offer.component.css',
 })
 export class AddOfferComponent {
   count: number = 1;
   zero: boolean = false;
+  benefit: boolean = false;
   selectedRegion?: string = '';
+  selectedOption?: string;
+
   addCount(event: Event) {
     event.preventDefault();
     if (this.count >= 9) {
@@ -28,5 +32,8 @@ export class AddOfferComponent {
     if (this.count > 1) {
       this.count -= 1;
     }
+  }
+  onSelectChange() {
+    this.benefit = !this.benefit;
   }
 }
